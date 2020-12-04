3783f67ad4e6f409f9e31c9fd5dc19cd73c53873
jasurbek <jasurbek@gmail.com>
2020-12-04 12:49:21 +0500

    test

---
+++ script.js
@@ -0,0 +1,28 @@
+// var javob = prompt('misolni yeching.. '+'1 '+ '+' + ' 2 = ')
+
+var name = prompt('Enter your name..')
+
+console.log('Enter your name... ' + 'My name is ' + name);
+
+var age = prompt('Enter your age..')
+
+console.log('Enter your age.. ' + 'I am ' + age + ' years old');
+
+var example = prompt('Solve the example.. '+'78 ' + '* ' + '3 =')
+
+console.log('The answer to the example ' + '78 ' + '* ' + '3 = ' + (78 * 3 )  + ' your answer is ' + example);
+
+var example = prompt('Solve the example.. ' + '128 ' + '+ ' + '67 ' + '+ ' + '88 ' )
+
+console.log('The answer to the example ' + '128 ' + '+ ' + '67 ' + '+ ' + '88 = ' + (78 * 3) + ' your answer is ' + example);
+
+var example = prompt('Solve the example.. ' + '346 ' + '+ ' + '98 ' + '* ' + '146 ' + '% ' + '4 ' )
+
+console.log('The answer to the example ' + '346 ' + '+ ' + '98 ' + '* ' + '146 ' + '% ' + '4 = ' +  (78 * 3) + ' your answer is ' + example);
+
+var example = prompt('Solve the example.. ' + '1004 ' + '+ ' + '55 ' + '- ' + '146 ' + '% ' + '4 ' + '+ ' + '77' )
+
+console.log('The answer to the example ' + '1004 ' + '+ ' + '55 ' + '- ' + '146 ' + '% ' + '4 ' + '+ ' + '77  = ' + (78 * 3) + ' your answer is ' + example);
+alert('Please go to console to see the answer' )
+
+
